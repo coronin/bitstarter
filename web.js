5b84789cb98d15b1e295f01950ae64695ea35278
Liang Cai <ubuntu@node.pl4.me>
2013-07-06 05:31:33 +0000

    tweak web.js 3

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var express = require('express'),
   buf = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
-  response.send( buf.toString );
+  response.send( buf.toString() );
 });
 
 var port = process.env.PORT || 5000;
